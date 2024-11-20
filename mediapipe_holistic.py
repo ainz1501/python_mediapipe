9b3ec22a7b51e7163d8ed7bca901490270e0826d
ainz1501 <callofcthulhu2010@gmail.com>
2024-11-20 19:22:23 +0900

    add stereo image

--- mediapipe_holistic.py
+++ mediapipe_holistic.py
@@ -6,9 +6,9 @@ mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 
 # 画像を読み込む サイズは4284 × 5712
-image_path = '/Users/tokudataichi/Documents/python_mediapipe/IMG_4997.JPG'
+image_path = '/Users/tokudataichi/Documents/python_mediapipe/left0_image.JPG'
 image = cv2.imread(image_path)
-image2_path = '/Users/tokudataichi/Documents/python_mediapipe/IMG_4998.JPG'
+image2_path = '/Users/tokudataichi/Documents/python_mediapipe/right50_image.JPG'
 image2 = cv2.imread(image2_path)
 
 # BGRをRGBに変換（MediaPipeがRGBを期待するため）
@@ -59,14 +59,22 @@ def transform_result(results, image):
         pose_cam_landmarks = transform2camera(results.pose_landmarks, width, height)
         for index, cam_landmarks in enumerate(pose_cam_landmarks):
             print(f"pose_cam_landmarks[{index}].x : {cam_landmarks.x}")
+    else:
+        pose_cam_landmarks = None
     if results.left_hand_landmarks:
         left_hand_cam_landmarks = transform2camera(results.left_hand_landmarks, width, height)
         for index, cam_landmarks in enumerate(left_hand_cam_landmarks):
             print(f"left_hand_cam_landmarks[{index}].x : {cam_landmarks.x}")
+    else:
+        left_hand_cam_landmarks = None
     if results.right_hand_landmarks:
         right_hand_cam_landmarks = transform2camera(results.right_hand_landmarks, width, height)
         for index, cam_landmarks in enumerate(right_hand_cam_landmarks):
             print(f"right_hand_cam_landmarks[{index}].x : {cam_landmarks.x}")
+    else:
+        right_hand_cam_landmarks = None
+    
+    return pose_cam_landmarks, left_hand_cam_landmarks, right_hand_cam_landmarks
 
 # Holisticモジュールを使って画像を処理
 with mp_holistic.Holistic(static_image_mode=True) as holistic:
@@ -99,7 +107,10 @@ with mp_holistic.Holistic(static_image_mode=True) as holistic:
         print("right hand 2 through")
         mp_drawing.draw_landmarks(image2, results2.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
                                   mp_drawing.DrawingSpec(color=(0,0,255)), mp_drawing.DrawingSpec(color=(0,0,0)))  
-    transform_result(results, image)      
+    
+    # 出力したキーポイントをカメラ座標系に変換(x,yの値のみ)
+    pose_cam, left_hand_cam, right_hand_cam = transform_result(results, image) 
+    pose_cam2, left_hand_cam2, right_hand_cam2 = transform_result(results2, image2)     
         
 
 # 処理結果を表示
