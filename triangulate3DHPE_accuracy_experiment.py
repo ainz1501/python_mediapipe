89e21496aa5ddb0b40a00f34723a5d661cf0d9ad
ainz1501 <callofcthulhu2010@gmail.com>
2025-01-09 05:16:10 +0900

    images up

--- triangulate3DHPE_accuracy_experiment.py
+++ triangulate3DHPE_accuracy_experiment.py
@@ -6,93 +6,98 @@ from datetime import datetime
 import sys
 import json
 import time
+import glob
 
 # キャリブレーションファイル呼び出し
-calibration_file = open("/Users/tokudataichi/Documents/python_mediapipe/panoptic-toolbox/171204_pose1_sample/calibration_171204_pose1_sample.json")
+calibration_file = open("./panoptic-toolbox/171204_pose1_sample/calibration_171204_pose1_sample.json")
 parameters = json.load(calibration_file)
-print(parameters["cameras"][0])
+print("cam_params:", not(parameters is None))
 
 # MediaPipe Holisticモジュールを初期化
 mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-# 入力
+# 入力動画
+VIDEO1 = cv2.VideoCapture("./panoptic-toolbox/171204_pose1_sample/hdVideos/hd_00_00.mp4")
+VIDEO2 = cv2.VideoCapture("./panoptic-toolbox/171204_pose1_sample/hdVideos/hd_00_01.mp4")
+# 内部パラメータ、外部パラメータ
 param1 = parameters["cameras"][479] # HDカメラ00_00
 param2 = parameters["cameras"][480]
 K_left = np.array(param1["K"])
+R_left, T_left = np.array(param1["R"]), np.array(param1["t"])
 K_right = np.array(param2["K"])
-VIDEO1 = cv2.VideoCapture("/Users/tokudataichi/Documents/python_mediapipe/panoptic-toolbox/171204_pose1_sample/hdVideos/hd_00_00.mp4")
-VIDEO2 = cv2.VideoCapture("/Users/tokudataichi/Documents/python_mediapipe/panoptic-toolbox/171204_pose1_sample/hdVideos/hd_00_01.mp4")
+R_right, T_right = np.array(param2["R"]), np.array(param2["t"])
+
 
 # レンダリング用入力
+# 一時的に画像を保存するフォルダパス
+TEMPORARY_IMAGES_STORAGE_PATH = "./output_images/images_temporary_storage/"
+FRAMES_STORAGE_PATH = "./output_images/hdvideo_frames_00&01/"
 # ボーン情報
 POSE_CONNECTIONS = mp_pose.POSE_CONNECTIONS
-
 # レンダリング情報
 JOINT_STYLE = mp_drawing.DrawingSpec(color=(0,0,255), thickness=5, circle_radius=3)
 BONE_STYLE = mp_drawing.DrawingSpec(color=(200,200,0), thickness=5)
 
 # 処理関数
-def Landmark_detect(image_left, image_right, matching_flag):
+def Landmark_detect(image_left, image_right):
     """
-    左右画像のランドマークを推定し、画像座標系のそれぞれのランドマークとマッチングリストを返す
+    左右画像それぞれの対象の手、顔、全身の3Dポーズを推定する
 
     Parameters:
     image_left (MatLike)        : 左カメラの画像
     image_right (MatLike)       : 右カメラの画像
-    matching_flag (Bool)        : ランドマークの選定を行うかどうかを決めるフラッグ変数
 
     Returns:
-    pose_left (numpy.ndarray)   : 33行2列の左画像の全身の画像座標系ランドマーク
-    pose_right (numpy.ndarray)  : 33行2列の右画像の全身の画像座標系ランドマーク
-    matchlist (list)            : 左右で規定値以上の信頼度をもつ対応するランドマークが存在することを示すマッチングリスト
+    result_left (MediaPipe独自クラス)   : 左画像の3Dポーズ
+    result_right (MediaPipe独自クラス)  : 右画像の3Dポーズ
     """
     # RGBスケールに変換
     image_leftRGB = cv2.cvtColor(image_left, cv2.COLOR_BGR2RGB)
     image_rightRGB = cv2.cvtColor(image_right, cv2.COLOR_BGR2RGB)
 
-    # ランドマーク推定 静止画 => static_image_mode=True 動画 => 
+    # ランドマーク推定 静止画入力 -> static_image_mode=True
     with mp_holistic.Holistic(static_image_mode=True) as holistic:
         result_left = holistic.process(image_leftRGB)
         result_right = holistic.process(image_rightRGB)
 
-    pose_left = Normalized_to_screen_coord(result_left.pose_landmarks, WIDTH, HEIGHT)
-    pose_right = Normalized_to_screen_coord(result_right.pose_landmarks, WIDTH, HEIGHT)
-    print("left=","\n",pose_left)
-    print("right=","\n",pose_right)
+    return result_left, result_right
 
-    matching_list = Matching_landmarks(result_left.pose_landmarks, result_right.pose_landmarks, enable=matching_flag)
-
-    # 推定結果を画像に描写
-    annotation_image(image_left, result_left.pose_landmarks, POSE_CONNECTIONS, JOINT_STYLE, BONE_STYLE)
-    # annotation_image(image_right, result_right.pose_landmarks, POSE_CONNECTIONS, JOINT_STYLE, BONE_STYLE)
-
-    return pose_left, pose_right, matching_list
-
-def Normalized_to_screen_coord(result, width, height):
+def Normalized_to_screen_coord(result_L, result_R, width, height):
     """
     左右画像で推定したランドマークを画像座標系（スクリーン座標系）の値に変換する
 
     Parameters:
-    result (NamedTuple)             : 推定したランドマークのリスト。result.landmark.x各ランドマークのx座標の値を取り出せる
-    width (int)                     : 画像の幅
-    height (int)                    : 画像の高さ
+    result (NamedTuple)   : 推定したランドマークのリスト。.landmark.x各ランドマークのx座標の値を取り出せる
+    width (int)           : 画像の幅
+    height (int)          : 画像の高さ
 
     Returns:
     scr_landmarks (numpy.ndarray)   : n行2列の画像座標系ランドマーク (全身なのでn=33)
     """
-    scr_list = [] # カメラ座標を記録する配列を生成
-    for index, landmark in enumerate(result.landmark):
+    scr_list_L = [] # カメラ座標を記録する配列を生成
+    scr_list_R = []
+    # 左側画像のランドマークを変換
+    for index, landmark in enumerate(result_L.landmark):
         # キーポイントの値をカメラ座標系（画像中央が原点）に変換
         scr_x = landmark.x * width
         scr_y = landmark.y * height
         # リストに挿入
-        scr_list.append([scr_x, scr_y]) 
-    
-    scr_landmarks = np.array(scr_list)
+        scr_list_L.append([scr_x, scr_y]) 
 
-    return scr_landmarks
+    # 右側画像のランドマークを変換
+    for index, landmark in enumerate(result_R.landmark):
+        # キーポイントの値をカメラ座標系（画像中央が原点）に変換
+        scr_x = landmark.x * width
+        scr_y = landmark.y * height
+        # リストに挿入
+        scr_list_R.append([scr_x, scr_y]) 
+    
+    scr_landmarks_L = np.array(scr_list_L)
+    scr_landmarks_R = np.array(scr_list_R)
+
+    return scr_landmarks_L, scr_landmarks_R
 
 def Matching_landmarks(landmark_left, landmark_right, enable):
     """
@@ -119,7 +124,7 @@ def Matching_landmarks(landmark_left, landmark_right, enable):
 
     return matching_list
 
-def Cam_pose_estimate(landmark_left, landmark_right, matchlist, K):
+# def Cam_pose_estimate(landmark_left, landmark_right, matchlist, K):
     """
     左右画像の対応ランドマークからカメラポーズを推定
 
@@ -177,7 +182,7 @@ def Projection_mat_calc(K_left, R_left, T_left, K_right, R_right, T_right):
 
     return Pleft, Pright
 
-def Triangulate_3Dpoint(Pleft, Pright, landmark_left, landmark_right, matchlist):
+def Triangulate_3Dpoint(Pleft, Pright, landmark_left, landmark_right):
     """
     カメラの投影行列を使用して特徴点の3D位置を再構築する
 
@@ -189,37 +194,56 @@ def Triangulate_3Dpoint(Pleft, Pright, landmark_left, landmark_right, matchlist)
     matches (list of int)           : 画像間で対応するランドマークの有無を示すリスト
 
     Returns:
-    points3D (numpy.ndarray)        : 再構築された3Dポイントの配列
+    points3D (numpy.ndarray)        : 再構築されたn行3列の3Dポイントの配列
     """
     # マッチングされた特徴点を取得
-    left_list = []
-    right_list = []
-    for idx, match in enumerate(matchlist):
-        if match == 1:
-            left_list.append(landmark_left[idx])
-            right_list.append(landmark_right[idx])
-    left_pts = np.array(left_list).T
-    right_pts = np.array(right_list).T
+    # left_list = []
+    # right_list = []
+    # for idx, match in enumerate(matchlist):
+    #     if match == 1:
+    #         left_list.append(landmark_left[idx])
+    #         right_list.append(landmark_right[idx])
+    # left_pts = np.array(left_list).T
+    # right_pts = np.array(right_list).T
     
     # 特徴点の3D位置を再構築　（4行n列）
-    points4D = cv2.triangulatePoints(Pleft, Pright, left_pts, right_pts)
+    points4D = cv2.triangulatePoints(Pleft, Pright, landmark_left.T, landmark_right.T)
     
-    # 同次座標を3D座標に変換
+    # 同次座標を3D座標に変換(3行n列)
     points3D = points4D[:3] / points4D[3] # 3行n列
 
-    landmark3D = np.zeros((len(matchlist), 3))
-    i = 0 # イテレーター
-    for idx, match in enumerate(matchlist):
-        if match == 1:
-            landmark3D[idx] = points3D.T[i]
-            i+=1
+    # landmark3D = np.zeros((len(matchlist), 3))
+    # i = 0 # イテレーター
+    # for idx, match in enumerate(matchlist):
+    #     if match == 1:
+    #         landmark3D[idx] = points3D.T[i]
+    #         i+=1
 
-    return landmark3D
+    return points3D.T
 
-# 誤差計算用
+def Triangulate3DHPE(img1, img2, K_l, R_l, T_l, K_r, R_r, T_r):
+    # 高さ、幅（同じカメラを用いるため片方の画像から取得）
+    HEIGHT, WIDTH, _ = img1.shape
+
+    # ランドマーク、マッチングリスト
+    result_left, result_right = Landmark_detect(image_left=img1, image_right=img2)
+    
+    # 画像座標系への変換(同時に体のランドマークのみ使用)
+    pose_left, pose_right = Normalized_to_screen_coord(result_left.pose_landmarks, result_right.pose_landmarks, WIDTH, HEIGHT)
+    print("left=","\n",pose_left)
+    print("right=","\n",pose_right)
+
+    # 投影行列を計算
+    Pleft, Pright = Projection_mat_calc(K_l, R_l, T_l, K_r, R_r, T_r)
+
+    # 3次元位置を復元
+    landmark3D = Triangulate_3Dpoint(Pleft, Pright, pose_left, pose_right)
+    print("landmark3D:","\n",landmark3D)
+
+    return result_left, result_right, landmark3D
 
 # 結果表示用関数
-def annotation_image(image1, landmarks1, connections, jointstyle, bonestyle):
+def annotate_image(image_L, landmarks_L, image_R, landmarks_R, connections, jointstyle, bonestyle):
     """
     mediapipeが推定したランドマークを画像に描画する関数
 
@@ -229,11 +253,21 @@ def annotation_image(image1, landmarks1, connections, jointstyle, bonestyle):
     connections (List of List of int)       : ランドマークを繋ぐボーンの連結情報。連結する2つのランドマークの数字がリスト化されている
     jointstyle (mp_drawing.DrawingSpec)     : ランドマークを示す点の描写情報
     bonestyle (mp_drawing.DrawingSpec)      : ランドマークを繋ぐ線の描写情報
+
+    Return:
+    original_images (list of MatLike)  : 注釈前の元の画像 (0:左カメラ画像, 1:右カメラ画像)
+    annotated_images (list of MatLike) : 注釈後の画像
     """
-    mp_drawing.draw_landmarks(image1, landmarks1, connections, 
+    cv2.imwrite(TEMPORARY_IMAGES_STORAGE_PATH+"original_L.png", image_L)
+    cv2.imwrite(TEMPORARY_IMAGES_STORAGE_PATH+"original_R.png", image_R)
+    # cv2.imshow("L original_image", original_images[0])
+    mp_drawing.draw_landmarks(image_L, landmarks_L, connections, 
                             jointstyle, bonestyle)
-    # cv2.imshow('Holistic Result', image)
-    # cv2.waitKey(0)
+    mp_drawing.draw_landmarks(image_R, landmarks_R, connections, 
+                        jointstyle, bonestyle)  
+    cv2.imwrite(TEMPORARY_IMAGES_STORAGE_PATH+"annotated_L.png", image_L)
+    cv2.imwrite(TEMPORARY_IMAGES_STORAGE_PATH+"annotated_R.png", image_R)
+    # cv2.imshow("L annotated_image", annotated_images[0])
 
 def plot_2Dskeleton(landmarks, connection):
     x_coords = []
@@ -258,12 +292,10 @@ def plot_2Dskeleton(landmarks, connection):
     plt.axis('equal')         # アスペクト比を正確にする
     plt.show()
 
-def plot_3Dskeleton(landmarks, connections, matchlist, frame_num):
+def plot_3Dskeleton(landmarks, connections, matchlist, save_path):
     fig = plt.figure(figsize = (8, 8))
     ax= fig.add_subplot(111, projection='3d')
     ax.scatter(landmarks[:, 0], landmarks[:,1],landmarks[:,2], s = 1, c = "blue")
-    # for i in range(len(landmarks[:, 0])):
-    #     ax.text(landmarks[i,0], landmarks[i,1], landmarks[i,2], str(i), dir=None, color="black", fontsize=8, ha='right', va='bottom')
     set_equal_aspect(ax)
     # 骨格情報からボーンを形成
     for connection in connections:
@@ -278,9 +310,11 @@ def plot_3Dskeleton(landmarks, connections, matchlist, frame_num):
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
     plt.title("3D Skeleton Visualization")
+    # 対象を正面から見た3Dプロットを画像として保存
     ax.view_init(elev=180, azim=5, roll=-90)
-    fig.savefig("/Users/tokudataichi/Documents/python_mediapipe/output_3dplot/3dplot_"+str(frame_num)+".png")
-    # plt.show()
+    fig.savefig(save_path+"3dplot.png")
+
+    return fig, ax
 
 # プロットの軸を等間隔にする
 def set_equal_aspect(ax):
@@ -303,43 +337,92 @@ def set_equal_aspect(ax):
      # ボックスアスペクト比を均等に設定 (バージョン 3.4+)
     ax.set_box_aspect([1, 1, 1])  # x, y, z を同じスケールに
 
+def concatenate_images(frame_num, storage_path, save_path):
+    # 注釈前・注釈後の画像の読み込み
+    original_images = (cv2.imread(storage_path+"original_L.png"), cv2.imread(storage_path+"original_R.png"))
+    annotated_images = (cv2.imread(storage_path+"annotated_L.png"), cv2.imread(storage_path+"annotated_R.png"))
+    HEIGHT, _, _ = original_images[0].shape # 画像高さ取得. HEIGHT=1080, WIDTH=1920
+
+    # 3Dプロットの読み込みとリサイズ
+    plot3d_image = cv2.imread(storage_path+"3dplot.png")
+    plot3d_image_resize = cv2.resize(plot3d_image, (HEIGHT, HEIGHT), interpolation=cv2.INTER_CUBIC)
+    # 画像を横に連結
+    concatenation_left_image = cv2.hconcat([original_images[0], annotated_images[0], plot3d_image_resize])
+    concatenation_right_image = cv2.hconcat([original_images[1], annotated_images[1], plot3d_image_resize])
+    # cv2.imshow("concatenation_image", concatenation_image)
+
+    frames_left_name = "frame_left_"+str(frame_num).zfill(4)+".jpg" # 例："frame_left_0001.jpg"
+    frames_right_name = "frame_right_"+str(frame_num).zfill(4)+".jpg" # 例："frame_right_0001.jpg"
+    cv2.imwrite(save_path+frames_left_name, concatenation_left_image)
+    cv2.imwrite(save_path+frames_right_name, concatenation_right_image)
+
+def create_video_from_images(storage_path):
+    # 左カメラ各フレームの比較用画像読み込み
+    frames_left = []
+    for name in sorted(glob.glob(storage_path+'*left*.jpg')):
+        img_left = cv2.imread(name)
+        if img_left is None:
+            break
+        print(name)
+        frames_left.append(img_left)
+    
+    # 右カメラ各フレームの比較用画像読み込み
+    frames_right = []
+    for name in sorted(glob.glob(storage_path+'*right*.jpg')):
+        img_right = cv2.imread(name)
+        if img_right is None:
+            break
+        print(name)
+        frames_right.append(img_right)
+
+    HEIGHT, WIDTH, _ = frames_left[0].shape
+    out_Lvideo = cv2.VideoWriter("output_left_video.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 10, (WIDTH, HEIGHT))
+    out_Rvideo = cv2.VideoWriter("output_right_video.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 10, (WIDTH, HEIGHT)) 
+    
+    for i in range(len(frames_left)):
+        out_Lvideo.write(frames_left[i])
+        out_Rvideo.write(frames_right[i])
+# create_video_from_images(storage_path=FRAMES_STORAGE_PATH)
 """
 -------------------------------------------------------------------------------------------------------
 """
 # メイン処理部
 frame_num = 1
 while True:
+    print("frame "+str(frame_num)) # 現在フレームを表示
     # ビデオキャプチャー
     ret1, img1 = VIDEO1.read()
     ret2, img2 = VIDEO2.read()
-
     if not (ret1 and ret2):
         print("breaked frame:", frame_num)
         break
+    
+    # 三角測量を用いた3Dポーズ推定(HPE)
     # 高さ、幅（同じカメラを用いるため片方の画像から取得）
-    HEIGHT, WIDTH, _ = img1.shape
-
-    # 注釈前の画像を保存する
-    cv2.imwrite("/Users/tokudataichi/Documents/python_mediapipe/output_images/hd00_"+str(frame_num)+".png", img1)
-
+    HEIGHT, WIDTH, _ = img1.shape   
     # ランドマーク、マッチングリスト
-    pose_left, pose_right, matchlist = Landmark_detect(img1, img2, matching_flag=False)
-
-    cv2.imwrite("/Users/tokudataichi/Documents/python_mediapipe/output_images/hd00_"+str(frame_num)+"_annotated.png", img1)
-    # 回転行列、並進ベクトル
-    R_left, T_left = np.array(param1["R"]), np.array(param1["t"])
-    R_right, T_right = np.array(param2["R"]), np.array(param2["t"])
-    # 投影行列
+    result_left, result_right = Landmark_detect(image_left=img1, image_right=img2)
+    # 画像座標系への変換(同時に体のランドマークのみ使用)
+    pose_left, pose_right = Normalized_to_screen_coord(result_left.pose_landmarks, result_right.pose_landmarks, WIDTH, HEIGHT)
+    print("left=","\n",pose_left)
+    print("right=","\n",pose_right)
+    # 投影行列を計算
     Pleft, Pright = Projection_mat_calc(K_left, R_left, T_left, K_right, R_right, T_right)
-    # 3次元位置
-    landmark3D = Triangulate_3Dpoint(Pleft, Pright, pose_left, pose_right, matchlist)
+    # 3次元位置を復元
+    landmark3D = Triangulate_3Dpoint(Pleft, Pright, pose_left, pose_right)
     print("landmark3D:","\n",landmark3D)
 
-    # レンダリング処理
-    # plot_2Dskeleton(pose_left, POSE_CONNECTIONS)
-    # plot_2Dskeleton(pose_right, POSE_CONNECTIONS)
-    plot_3Dskeleton(landmark3D, POSE_CONNECTIONS, matchlist, frame_num)
-
-    time.sleep(1)
-    cv2.destroyAllWindows() 
+    # 比較用画像を作成
+    annotate_image(img1, result_left.pose_landmarks, img2, result_right.pose_landmarks, 
+                                                              POSE_CONNECTIONS, JOINT_STYLE, BONE_STYLE)
+    matching_list = Matching_landmarks(result_left.pose_landmarks, result_right.pose_landmarks, enable=False)
+    fig, ax = plot_3Dskeleton(landmark3D, POSE_CONNECTIONS, matching_list, save_path=TEMPORARY_IMAGES_STORAGE_PATH)
+    # 比較用画像を横に連結する
+    concatenate_images(frame_num, storage_path=TEMPORARY_IMAGES_STORAGE_PATH, save_path=FRAMES_STORAGE_PATH)
+    
     frame_num += 1
+    # cv2.waitKey(0)
+    cv2.destroyAllWindows() 
+
+# 比較用動画の作成
+create_video_from_images(FRAMES_STORAGE_PATH)
